534e1d0c7509424ab8150613f2988eff71d8b484
Abduraxmon Asatullayev <abduraxmonasatullayev35@gmail.com>
2022-03-11 22:35:19 +0500

    add html

--- app/api.py
+++ app/api.py
@@ -4,6 +4,14 @@ from .models import City
 from .forms import CityForm
 
 
+def main(request):
+    return render(request, 'weather/main.html')
+
+
+def info(request):
+    return render(request, 'weather/info.html')
+
+
 def index(request):
     appid = '846801b52dfcdf6d8d840177d4bc5397'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
@@ -23,11 +31,13 @@ def index(request):
         city_info = {
             'city': city.name,
             'temp': res["main"]["temp"],
-            'icon': res["weather"][0]["icon"]
+            'icon': res["weather"][0]["icon"],
+            'description': res['weather'][0]['description'],
+            'speed': res['wind']['speed']
         }
 
         all_cities.append(city_info)
 
-    context = {'all_info': all_cities, 'form': form}
+        context = {'all_info': all_cities, 'form': form}
 
-    return render(request, 'weather/index.html', context)
+        return render(request, 'weather/index.html', context)
--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import api
 
 urlpatterns = [
     path('index.html', api.index),
+    path('main.html', api.main),
+    path('info.html', api.info)
 ]
